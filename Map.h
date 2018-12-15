7b291ebcaadf6bacc0862bff2a2669f0652823b3
krystiano365 <krystiano365@gmail.com>
2018-12-15 18:38:25 +0100

    emergency commit

--- Map.h
+++ Map.h
@@ -5,4 +5,37 @@
 #ifndef MAP_MAP_H
 #define MAP_MAP_H
 
+template<class key_t, class val_t> class Map{
+
+private:
+	struct Pair{
+	private:
+		key_t key;
+		val_t value;
+	public:
+		Pair(key_t k, val_t v){
+			key = k;
+			value = v;
+		}
+		~Pair()=default;
+		key_t getKey(){
+			return key;
+		}
+		val_t getVal(){
+			return value;
+		}
+	};
+
+	Pair *array;
+
+public:
+	Map(key_t key, val_t val){
+		array = new Pair(key, val);
+
+	}
+};
+
+
+
+
 #endif //MAP_MAP_H
