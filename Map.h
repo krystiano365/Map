cb5a82d275ab1538e05bb3ad70626050c0ec82d5
krystiano365 <krystiano365@gmail.com>
2018-12-16 02:01:18 +0100

    emergency commit

--- Map.h
+++ Map.h
@@ -5,36 +5,43 @@
 #ifndef MAP_MAP_H
 #define MAP_MAP_H
 
-template<class key_t, class val_t> class Map{
+#define SIZE 2
 
-private:
+template<class key_t, class val_t> class Map{
 	struct Pair{
-	private:
 		key_t key;
 		val_t value;
-	public:
+		Pair *nextPair;
 		Pair(key_t k, val_t v){
 			key = k;
 			value = v;
+			nextPair = nullptr;
 		}
 		~Pair()=default;
-		key_t getKey(){
-			return key;
-		}
-		val_t getVal(){
-			return value;
-		}
 	};
-
-	Pair *array;
-
+private:
+	unsigned int elements, length;
+	Pair **array;
 public:
-	Map(key_t key, val_t val){
-		array = new Pair(key, val);
-
+	Map(){
+		array = new Pair*[1];
+		*array = nullptr;
+		elements = 0;
+		length = 1;
 	}
+	~Map(){
+		for(int i = 0; i < length; ++i){
+			Pair *curr = array[i];
+			while(curr != nullptr){
+				Pair *temp = curr->nextPair;
+				delete curr;
+				curr = temp;
+			}
+		}
+		delete[]array;
+	}
+	void add();
 };
-
 
 
 
